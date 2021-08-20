73ab340c26ba3210e9495c512645ef355e310cf1
Anchal Pandey <73632448+code1511@users.noreply.github.com>
2021-08-20 11:29:30 +0530

    Update uglynumbers.cpp
    https://leetcode.com/problems/ugly-number/

--- DP/uglynumbers.cpp
+++ DP/uglynumbers.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int check(int a,int b){
+        while(a%b==0){
+            a=a/b;
+        }
+        return a;
+    }
+    bool isUgly(int n) {
+        if(n==0) return false;
+        n = check(n,2);        
+        n = check(n,3);
+        n = check(n,5);
+       
+     return (n==1)?1:0;
+    }
+};
